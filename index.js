5e8edc743a5203dc6cf4be1fc8a362170dd722a4
Josephine Hall <josephinehall@gmail.com>
2014-09-09 11:03:30 +1000

    Add cents to pence conversion rates

--- index.js
+++ index.js
@@ -178,6 +178,31 @@ module.exports = {
       default:
         return 0;
     }
+  },
+
+  convertCentsToPence: function(value){
+    switch (value){
+      case 1:
+        return 1;
+      case 2:
+        return 3;
+      case 3:
+        return 4;
+      case 4:
+        return 5;
+      case 5:
+        return 6;
+      case 6:
+        return 7;
+      case 7:
+        return 8;
+      case 8:
+        return 10;
+      case 9:
+        return 11;
+      default:
+        return 0;
+    }
   }
 
 };
